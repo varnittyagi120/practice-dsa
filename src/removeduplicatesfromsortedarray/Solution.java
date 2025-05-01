f750ca54dbdeda18be439b88ae3626084e91e1fe
varnittyagi120 <varnittyagi1204@gmail.com>
2025-05-02 02:37:43 +0530

    remove duplicates from sorted array

---
+++ src/removeduplicatesfromsortedarray/Solution.java
@@ -0,0 +1,25 @@
+package src.removeduplicatesfromsortedarray;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i!=nums.length-1){
+                if(nums[i]==nums[i+1]){
+                    nums[i] = -200;
+                }
+            }
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=-200){
+                if(i!=result){
+                    nums[result] = nums[i];
+                    nums[i] = -200;
+                }
+                result++;
+            }
+        }
+        return result;
+    }
+}
