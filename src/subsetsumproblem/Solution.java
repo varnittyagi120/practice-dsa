f9c6797431f20930cdd22f79991aba77d244037e
varnittyagi120 <varnittyagi1204@gmail.com>
2025-05-26 02:00:33 +0530

    subset sum problem

---
+++ src/subsetsumproblem/Solution.java
@@ -0,0 +1,31 @@
+package src.subsetsumproblem;
+
+public class Solution {
+    static Boolean isSubsetSum(int arr[], int sum) {
+        int n = arr.length+1;
+        int m = sum+1;
+        boolean[][] t = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0){
+                    t[i][j] = false;
+                }
+                if(j==0){
+                    t[i][j] = true;
+                }
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i-1]<=j){
+                    t[i][j] = (t[i-1][j-arr[i-1]] || t[i-1][j]);
+                } else {
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[arr.length][sum];
+
+    }
+}
