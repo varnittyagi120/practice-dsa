f1b726a1c265a6b6597d804a46d1fcb3e390fc86
varnittyagi120 <varnittyagi1204@gmail.com>
2025-06-05 12:02:45 +0530

    longest palindromic substring

---
+++ src/longestpalindromicsubstring/Solution.java
@@ -0,0 +1,40 @@
+package src.longestpalindromicsubstring;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length()==1){
+            return s;
+        }
+        String LPS = "";
+        for(int i=1;i<s.length();i++){
+            int low = i;
+            int high = i;
+            while(s.charAt(low)==s.charAt(high)){
+                low = low-1;
+                high = high+1;
+                if(low==-1 || high>=s.length()){
+                    break;
+                }
+            }
+            String palindrome = s.substring(low+1, high);
+            if(palindrome.length()>LPS.length()){
+                LPS = palindrome;
+            }
+
+            low = i-1;
+            high = i;
+            while(s.charAt(low)==s.charAt(high)){
+                low = low-1;
+                high = high+1;
+                if(low==-1 || high>=s.length()){
+                    break;
+                }
+            }
+            palindrome = s.substring(low+1, high);
+            if(palindrome.length()>LPS.length()){
+                LPS = palindrome;
+            }
+        }
+        return LPS;
+    }
+}
