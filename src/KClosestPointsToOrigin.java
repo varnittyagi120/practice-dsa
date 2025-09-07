91d27d13412db8d2433166ae0c85da9ab92dd427
varnittyagi120 <varnittyagi1204@gmail.com>
2025-09-07 19:49:22 +0530

    kth closest point to the origin

--- src/KClosestPointsToOrigin.java
+++ src/KClosestPointsToOrigin.java
@@ -2,43 +2,47 @@ package src;
 
 import java.util.PriorityQueue;
 
+//time complexity : O(nlog(k))
+// space complexity : O(K)
 public class KClosestPointsToOrigin {
 
-    class Coordinate{
+    class Coordinate {
         int[] coordinate;
         double distance;
-        public Coordinate(int[] coordinate, double distance){
+
+        public Coordinate(int[] coordinate, double distance) {
             this.coordinate = coordinate;
             this.distance = distance;
         }
-        public double getDistance(){
+
+        public double getDistance() {
             return distance;
         }
 
-        public int[] getCoordinate(){
+        public int[] getCoordinate() {
             return coordinate;
         }
     }
 
-        public int[][] kClosest(int[][] points, int k) {
-            //max heap
-            PriorityQueue<Coordinate> queue = new PriorityQueue<>((a, b)->(Double.compare(b.getDistance(), a.getDistance())));
-            for(int i=0;i<points.length;i++){
-                double sum = Math.pow(points[i][0], 2)+ Math.pow(points[i][1], 2);
-                double distance = Math.sqrt(sum);
-                Coordinate coordinate = new Coordinate(points[i], distance);
-                queue.add(coordinate);
-                if(queue.size()>k){
-                    queue.poll();
-                }
+    public int[][] kClosest(int[][] points, int k) {
+        //max heap
+        PriorityQueue<Coordinate> queue = new PriorityQueue<>((a, b) -> (Double.compare(b.getDistance(), a.getDistance())));
+        for (int i = 0; i < points.length; i++) {
+            double sum = Math.pow(points[i][0], 2) + Math.pow(points[i][1], 2);
+            double distance = Math.sqrt(sum);
+            Coordinate coordinate = new Coordinate(points[i], distance);
+            queue.add(coordinate);
+            if (queue.size() > k) {
+                queue.poll();
             }
-            int[][] result = new int[k][2];
-            int i = 0;
-            while(!queue.isEmpty()){
-                Coordinate coordinate = queue.poll();
-                result[i] = coordinate.getCoordinate();
-                i++;
-            }
-            return result;
         }
+        int[][] result = new int[k][2];
+        int i = 0;
+        while (!queue.isEmpty()) {
+            Coordinate coordinate = queue.poll();
+            result[i] = coordinate.getCoordinate();
+            i++;
+        }
+        return result;
+    }
 }
