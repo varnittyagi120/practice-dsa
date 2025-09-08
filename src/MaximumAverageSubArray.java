64e1342ff8b14be32a3d1561c115856fe244dfef
varnittyagi120 <varnittyagi1204@gmail.com>
2025-09-08 23:46:40 +0530

    maximum average subarray

---
+++ src/MaximumAverageSubArray.java
@@ -0,0 +1,23 @@
+package src;
+
+public class MaximumAverageSubArray {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum = sum + nums[i];
+            if(i+1 == k){
+                if(sum>max){
+                    max = sum;
+                }
+            } else if(i+1>k) {
+                sum = sum-nums[i-k];
+                if(sum>max){
+                    max = sum;
+                }
+            }
+        }
+        double result = (double)max/(double)k;
+        return result;
+    }
+}
