b171fca30cd922141a5f1420b88583caa626b642
varnittyagi120 <varnittyagi1204@gmail.com>
2025-09-30 22:37:12 +0530

    reverse a string using recursion

---
+++ src/ReverseAStringUsingRecursion.java
@@ -0,0 +1,19 @@
+package src;
+
+public class ReverseAStringUsingRecursion {
+        public void reverseString(char[] s) {
+            reverseAString(s, 0, s.length-1);
+        }
+
+        private void reverseAString(char[] input, int left, int right){
+            if(left>right){
+                return;
+            }
+            char ch = input[left];
+            input[left] = input[right];
+            input[right] = ch;
+            left++;
+            right--;
+            reverseAString(input, left, right);
+        }
+}
