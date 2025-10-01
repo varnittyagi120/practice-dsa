49d2a4b91ff375f6c515deb7fd1b8909de23d67a
varnittyagi120 <varnittyagi1204@gmail.com>
2025-10-02 02:39:48 +0530

    power of three

---
+++ src/PowerOfThree.java
@@ -0,0 +1,18 @@
+package src;
+
+public class PowerOfThree {
+        public boolean isPowerOfThree(int n) {
+            if(n==1){
+                return true;
+            }
+
+            if(n<=0){
+                return false;
+            }
+            int reminder = n%3;
+            if(reminder!=0){
+                return false;
+            }
+            return isPowerOfThree(n/3);
+        }
+}
