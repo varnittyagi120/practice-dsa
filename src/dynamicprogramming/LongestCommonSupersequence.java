0b293e5226b1b36d442e01262bf534e393dbb830
varnittyagi120 <varnittyagi1204@gmail.com>
2026-02-05 01:06:09 +0530

    longest common super sequence

---
+++ src/dynamicprogramming/LongestCommonSupersequence.java
@@ -0,0 +1,31 @@
+package src.dynamicprogramming;
+
+public class LongestCommonSupersequence {
+        public static int minSuperSeq(String s1, String s2) {
+            // code here
+            int m = s1.length();
+            int n = s2.length();
+            int[][] dp = new int[m+1][n+1];
+            for(int i=0;i<m+1;i++){
+                for(int j=0;j<n+1;j++){
+                    if(i==0 || j==0){
+                        dp[i][j] = 0;
+                    }
+                }
+            }
+
+            for(int i=1;i<m+1;i++){
+                for(int j=1;j<n+1;j++){
+                    if(s1.charAt(i-1)==s2.charAt(j-1)){
+                        dp[i][j] = 1+dp[i-1][j-1];
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+
+            return (m+n-dp[m][n]);
+        }
+}
+
+//time complexity and space complexity O(m*n)
